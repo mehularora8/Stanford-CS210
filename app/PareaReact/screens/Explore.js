ea03d24b82278479a1b9d4942ec1df3364cdd23e
Taylor Lallas <tlallas@stanford.edu>
2022-02-24 16:52:22 -0800

    preliminary restructuring of explore page

--- app/PareaReact/screens/Explore.js
+++ app/PareaReact/screens/Explore.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Dimensions, ScrollView } from 'react-native';
+import { StyleSheet, Dimensions, ScrollView, Text } from 'react-native';
 import { Block, theme } from 'galio-framework';
 
 import { Card } from '../components';
@@ -12,14 +12,21 @@ class Home extends React.Component {
       <ScrollView
         showsVerticalScrollIndicator={false}
         contentContainerStyle={styles.articles}>
+          <Block>
+            {/* Insert map here and remove placeholder */}
+            <Text style={styles.mapTemp}>This is a placeholder for the map.</Text> 
+          </Block>
         <Block flex>
+          <Text style={styles.headerText}>Resources Near You</Text>
+
           <Card item={articles[0]} horizontal  />
-          <Block flex row>
+          {/* <Block flex row>
             <Card item={articles[1]} style={{ marginRight: theme.SIZES.BASE }} />
             <Card item={articles[2]} />
-          </Block>
+          </Block> */}
           <Card item={articles[3]} horizontal />
-          <Card item={articles[4]} full />
+          <Card item={articles[4]} horizontal />
+          {/* <Card item={articles[4]} full /> */}
         </Block>
       </ScrollView>
     )
@@ -42,6 +49,15 @@ const styles = StyleSheet.create({
     width: width - theme.SIZES.BASE * 2,
     paddingVertical: theme.SIZES.BASE,
   },
+  mapTemp: {
+    paddingVertical: theme.SIZES.BASE * 8,
+    backgroundColor: "#999999",
+    textAlign: "center",
+    marginBottom: "3%"
+  }, 
+  headerText: {
+    fontSize: 17,
+  }
 });
 
 export default Home;
