9a9ac781dfa7718b76c1325aa996563400ac6bc7
Taylor Lallas <tlallas@stanford.edu>
2022-03-03 21:22:52 -0800

    subratings with numeric values

--- app/PareaReact/components/ReviewSummaryCard.js
+++ app/PareaReact/components/ReviewSummaryCard.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
-import { StyleSheet, Dimensions, Image, TouchableWithoutFeedback} from 'react-native';
+import { StyleSheet, Dimensions, Image, TouchableWithoutFeedback, View} from 'react-native';
 import { Block, Text, theme } from 'galio-framework';
 import { AirbnbRating } from 'react-native-ratings';
 import { argonTheme } from '../constants';
 import { LinearProgress } from 'react-native-elements';
 import ProgressBar from 'react-native-progress/Bar';
+import { style } from 'dom-helpers';
 
 class ReviewSummaryCard extends React.Component {
   render() {
@@ -33,28 +34,48 @@ class ReviewSummaryCard extends React.Component {
                 <Text style={styles.subReviewText}  >
                     Safety
                 </Text>
-                <ProgressBar progress={0.95} width={200} color={argonTheme.COLORS.BLACK} height={4} />
+                <View style={styles.subReviewBarContainer}>
+                  <ProgressBar progress={0.97} width={200} color={argonTheme.COLORS.BLACK} height={4} />
+                  <Text style={styles.subReviewNumVal}>
+                      4.9
+                  </Text>
+                </View>
               </Block>
 
               <Block style={styles.subReviewContainer}>
                 <Text style={styles.subReviewText}>
                   Accessibility 
                   </Text>
-                  <ProgressBar progress={0.95} width={200} color={argonTheme.COLORS.BLACK} height={4} />
+                  <View style={styles.subReviewBarContainer}>
+                  <ProgressBar progress={0.88} width={200} color={argonTheme.COLORS.BLACK} height={4} />
+                  <Text style={styles.subReviewNumVal}>
+                      4.7
+                  </Text>
+                </View>
                 </Block>
 
                 <Block style={styles.subReviewContainer}>
                   <Text style={styles.subReviewText}>
                   Environment
                   </Text>
-                  <ProgressBar progress={0.85} width={200} color={argonTheme.COLORS.BLACK} height={4} />
+                  <View style={styles.subReviewBarContainer}>
+                  <ProgressBar progress={0.93} width={200} color={argonTheme.COLORS.BLACK} height={4} />
+                  <Text style={styles.subReviewNumVal}>
+                      4.8
+                  </Text>
+                </View>
                 </Block>
 
                 <Block style={styles.subReviewContainer}>
                   <Text style={styles.subReviewText}>
                   Communication
                   </Text>
-                  <ProgressBar progress={0.90} width={200} color={argonTheme.COLORS.BLACK} height={4} />
+                  <View style={styles.subReviewBarContainer}>
+                  <ProgressBar progress={0.93} width={200} color={argonTheme.COLORS.BLACK} height={4} />
+                  <Text style={styles.subReviewNumVal}>
+                      4.8
+                  </Text>
+                </View>
                 </Block>
 
               </Block>
@@ -98,6 +119,17 @@ const styles = StyleSheet.create({
     display: 'flex',
     alignSelf: "flex-start",
     marginBottom: 8,
+  }, 
+  subReviewBarContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+  }, 
+  subReviewNumVal: {
+    fontFamily: "Open Sans",
+    fontWeight: "bold",
+    fontSize: 11,
+    marginLeft: 4
   }
 });
 
