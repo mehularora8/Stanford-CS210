606ef378f75babbfcd23559d5754dd2e2778a510
Mehul <aroram8@stanford.edu>
2022-04-26 14:23:30 -0700

    push google places API -- doesn't work yet

--- app/PareaReact/screens/addResource.js
+++ app/PareaReact/screens/addResource.js
@@ -6,21 +6,46 @@ import {
   StatusBar,
   KeyboardAvoidingView,
   ScrollView,
-  View
+  View,
+  FlatList
 } from "react-native";
 import { Block, Checkbox, Text, theme } from "galio-framework";
 import { Feather } from '@expo/vector-icons';
 import { Button, Icon, Input } from "../components";
 import { Images, argonTheme } from "../constants";
 import AddResourceSuccess from "./AddResourceSuccess";
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 
 
 // import auth from '@react-native-firebase/auth';
 
 const { width, height } = Dimensions.get("screen");
 
+const GOOGLE_PLACES_BASE_URL = 'https://maps.googleapis.com/maps/api/place';
+
+export type PredictionType = {
+  description: string,
+  place_id: string,
+  reference: string,
+  matched_substrings: any[],
+  tructured_formatting: Object,
+  terms: Object[],
+  types: string[],
+}
+
 
 class AddResource extends React.Component {
+
+  state = {
+    address: '',
+    predictions: []
+  }
+
+
+  submitForReview = () => {
+
+  }
+  
   render() {
     const { navigation } = this.props;
     return (
@@ -29,16 +54,16 @@ class AddResource extends React.Component {
           <Block safe flex middle>
             <Block style={styles.registerContainer}>
               <Block flex>
-              <Button onPress={() => navigation.goBack()} style={styles.cancel} title="X" >
-                <Feather name="x" size={28} color="black" />	
-              </Button>
+                <Button onPress={() => navigation.goBack()} style={styles.cancel} title="X" >
+                  <Feather name="x" size={28} color="black" />	
+                </Button>
                 <Block flex middle style={{marginTop:20}}>
                   <Text color="#FC3901" size={17}>
                     Add a Resource
                   </Text>
                 </Block>
 
-                <ScrollView style={styles.scroll}>
+                <Block>
                   <Block flex middle>
                     <Text style={styles.text}
                       color={argonTheme.COLORS.BLACK}
@@ -48,7 +73,7 @@ class AddResource extends React.Component {
                     </Text>
                   </Block>
 
-                  <Block flex center>
+                  <Block style = {styles.scroll} flex center>
                     <KeyboardAvoidingView
                       style={{ flex: 1 }}
                       behavior="padding"
@@ -85,18 +110,17 @@ class AddResource extends React.Component {
                         />
                       </Block>
                       <Block width={width * 0.9} style={{ marginBottom: 10 }}>
-                        <Input
-                          borderless
-                          placeholder="Address"
-                          iconContent={
-                            <Icon
-                              size={16}
-                              color={argonTheme.COLORS.ICON}
-                              // name="map-big"
-                              family="ArgonExtra"
-                              style={styles.inputIcons}
-                            />
-                          }
+                        <GooglePlacesAutocomplete
+                          placeholder='Address'
+                          onPress={(data, details = null) => {
+                            // 'details' is provided when fetchDetails = true
+                            console.log("Autocomplete is being called")
+                            console.log("DATA:", data);
+                          }}
+                          query={{
+                            key: 'AIzaSyD5FIODfpTMIyy_fS2Jyj6oCYpNloq8jS0',
+                            language: 'en',
+                          }}
                         />
                       </Block>
                       <Block width={width * 0.9} style={{ marginBottom: 10 }}>
@@ -147,6 +171,7 @@ class AddResource extends React.Component {
                       <Block middle>
                         <Button onPress={() => {
                           navigation.goBack()
+                          this.submitForReview()
                           navigation.navigate(AddResourceSuccess)
                           }} color="primary" style={styles.createButton}>
                           <Text bold size={14} color={argonTheme.COLORS.WHITE}>
@@ -156,7 +181,7 @@ class AddResource extends React.Component {
                       </Block>
                     </KeyboardAvoidingView>
                   </Block>
-                </ScrollView>
+                </Block>
               </Block>
             </Block>
           </Block>
@@ -211,7 +236,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold'
   },
   scroll: {
-    marginVertical: 0,
+    marginVertical: -300,
     
   },
   text: {
